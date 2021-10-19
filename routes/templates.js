418d8799ee65bd3778974a8c1230abfc0ef9e0ab
ZuziaDev <89024894+ZuziaDev@users.noreply.github.com>
2021-10-19 23:37:56 +0300

    Delete templates.js

--- routes/templates.js
+++
@@ -1,8 +0,0 @@
-const deeznuts = require("../main/templates.json")
-module.exports = {
-  name: "templates/discord",
-  run: async(req, res) => {
-    let rand_deeznuts = deeznuts[Math.floor(Math.random() * deeznuts.length)]
-    res.json({ templates: rand_deeznuts })
-  }
-}
